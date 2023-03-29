a601adebb429a6468e452a19abcb451521947800
Barry <106887760+Barryugo@users.noreply.github.com>
2023-03-29 12:14:21 +0100

    Create twistream.py

---
+++ twistream.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import snscrape 
+import snscrape.modules.twitter as sntwitter
+
+st.title("Twitter User Scraper")
+
+account_name = st.text_input("Enter a Twitter account name")
+
+if st.button("Scrape"):
+
+user = next(sntwitter.TwitterUserScraper(account_name).get_items())
+
+st.write(f"{account_name} has {user.user.followersCount} followers and {user.user.favouritesCount} likes.")
