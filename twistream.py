d07d9111a245c8f97793a8b6196019fd65dc8185
Barry <106887760+Barryugo@users.noreply.github.com>
2023-03-29 12:20:39 +0100

    Update twistream.py

--- twistream.py
+++ twistream.py
@@ -8,6 +8,6 @@ account_name = st.text_input("Enter a Twitter account name")
 
 if st.button("Scrape"):
 
-user = next(sntwitter.TwitterUserScraper(account_name).get_items())
+  user = next(sntwitter.TwitterUserScraper(account_name).get_items())
 
 st.write(f"{account_name} has {user.user.followersCount} followers and {user.user.favouritesCount} likes.")
